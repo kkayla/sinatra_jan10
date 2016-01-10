ce0493fa1057d61e73db7e132e7da6097372882c
Kayla Williams <kkayla.williams@icloud.com>
2016-01-10 16:50:17 -0500

    routes are working, ned to create users to display content

--- app.rb
+++ app.rb
@@ -1,20 +1,34 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
- set :database, 'sqlite3:database.sqlite3'
+set :database, 'sqlite3:database.sqlite3'
 
- require './models'
+require './models'
 
+# the root route displays all of the posts
+# their is a typo on line 4 of index.erb  
 get '/' do
   @posts = Post.all
   erb :index
 end
-
 # this maps the / url to the template index.html (or index.erb) which is found in the "views/posts/" directory
 
-get '/onepost' do
-  @the_last_post = Post.last
-  erb :onepost
+
+get '/create' do
+  @post = Post.new
+  erb :create
+end
+get '/posts' do
+  # @posts = Post.find(1)
+    @posts = Post.new(params[:user])
+  erb :posts
 end
 
-# make a form on the welcome.erb view so the user can make a new post on their welcome page
+post '/posts' do
+  @post = Post.new(title: params[:title], body: params[:body])
+  if @post.save
+    redirect '/'
+  else
+    erb :create
+  end
+end
